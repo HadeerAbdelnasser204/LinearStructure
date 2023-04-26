96e8701baa5103aa2e6e13d1b8f69a26d6776449
Hadeer Abdelnasser <101938448+HadeerAbdelnasser204@users.noreply.github.com>
2023-04-26 16:39:09 +0200

    Update SLL.cpp

--- SLL.cpp
+++ SLL.cpp
@@ -206,3 +206,40 @@ void SingleLinkedList ::print() {
     }
     cout << endl;
 }
+//problems.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*________________________2)Merge________________________________*/
+
+class Solution {
+public:
+    Node* mergeTwoLists(Node* l1, Node* l2) {
+        Node* dummy = new Node(0);
+        Node* tail = dummy;
+        while (l1 && l2) {
+            if (l1->data < l2->data) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+        tail->next = l1 ? l1 : l2;
+        return dummy->next;
+    }
+};
